868d98501355f7c829d20e258342b498d743a3e0
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2023-05-18 19:07:38 -0300

    add processamento de arquivo

--- app/controllers/upload_transaction_file_controller.rb
+++ app/controllers/upload_transaction_file_controller.rb
@@ -3,5 +3,14 @@
 class UploadTransactionFileController < ApplicationController
   def index; end
 
-  def create; end
+  def create
+    file = params[:file]
+    if file.present?
+      CNAB::UploadFile.new(file).call
+      CNAB::ProcessFileJob.perform_later(file.path)
+      redirect_to transaction_index_path
+    else
+      redirect_to upload_transaction_file_index_path, alert: 'Selecione um arquivo'
+    end
+  end
 end
