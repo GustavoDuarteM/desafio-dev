d163c8d1a5d0cc34b5043aa6df030d9de7648982
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2023-05-18 22:44:25 -0300

    corrigir carregamento de tabela de transações

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -3,14 +3,23 @@ import consumer from "channels/consumer"
 
 // Connects to data-controller="transactions"
 export default class extends Controller {
-  static targets = [ "transactions" , 'table']
+  static targets = [ "transactions" , 'table', 'balance']
+  
+  chatChannel = null;
 
   connect() {
-    this.createChanel(this)
+    this.chatChannel = this.createChanel(this)
+  }
+
+  disconnect() {
+    this.chatChannel.unsubscribe();
   }
 
   createChanel(instance){
     return consumer.subscriptions.create({channel: "TransactionChannel", room: 'transaction_channel'}, {
+      connected() {
+        this.perform("retrieve_transactions");
+      },
       received(data) {
         instance.tableTarget.innerHTML = data
       }
--- app/services/CNAB/Interpreter_line.rb
+++ app/services/CNAB/Interpreter_line.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CNAB
   class InterpreterLine
     def initialize(line)
@@ -14,10 +16,10 @@ module CNAB
       {
         transaction_type: @line[0].to_i,
         date: @line[1..8].to_date,
-        value: (@line[9..18].to_f / 100_00),
+        value: (@line[9..18].to_f / 100),
         cpf: @line[19..29],
         card: @line[30..41],
-        hour: @line[42..47],
+        hour: @line[42..47].insert(2, ':').insert(5, ':'),
         store_owner: @line[48..61],
         store_name: @line[62..80]
       }
